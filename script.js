6f7af474b1441e477149fc555b2a74d60db5cc69
Funsho Fatoye <fatoyefunsho@gmail.com>
2020-04-19 23:58:33 +0100

    added array functionality

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function wordChange(intNum) {
 
+    let arr = [];
+
     for (let i = 1; i <= intNum; i++) {
-        // console.log(i);  
+       
+        arr.push(i);
         if(i%2 === 0){
-            i = "yu";
-            continue;
-        } 
-        console.log(i);
+            arr.push("yu");
+        } else if(i%3 === 0){
+            arr.push("gi");
+        } else if(i%5 === 0){
+            arr.push("oh");
+        } else {
+            arr.push(i);
+        }
+        // console.log(i);
     }
+    console.log(arr);
+    // return arr
 }
 
 wordChange(100);
